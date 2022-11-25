fd540417098d82a7520cb1c86b0c92355245fb5d
MilletMorgan <morgan.millet@bedrockstreaming.com>
2022-11-25 15:16:09 +0100

    feat: add prettier and eslint

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+import HelloWorld from "./components/HelloWorld.vue";</script>
 
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
-    
+
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -26,9 +25,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
